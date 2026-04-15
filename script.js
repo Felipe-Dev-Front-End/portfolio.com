1dfc16ffa59132a2872b95c9bc7f438b3f2ee826
Felipe-Dev-Front-End <luizfelipea445@gmail.com>
2026-04-15 20:23:15 -0300

    nova atualização responsiva fotos e titulos

---
+++ script.js
@@ -0,0 +1,25 @@
+// função para abrir menuMobile
+function openMenu() {
+  document.getElementById("menuMobile").classList.toggle("change");
+  document.getElementById("navLinks").classList.toggle("change");
+  document.getElementById("bg").classList.toggle("change");
+ 
+}
+// função para abrir menuMobile
+
+// função btn dark mode
+const chk = document.getElementById('chk');
+chk.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+})
+
+// função btn dark mode
+
+// função texto digitado
+new TypeIt("#typingText", {
+  speed: 100,
+  loop: true,
+  waintUntilVisible: true,
+}).type('Desenvolvedor Front-End', { delay: 2000 }).delete(9).type(' Web Designer', { delay: 2000 })
+  .go();
+// função texto digitado
